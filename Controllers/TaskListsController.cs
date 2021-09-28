f081a70b27010902cdb838327eb75f68a905a2fa
Cassly Sullen <csulleniii@gmail.com>
2021-09-28 01:29:18 -0400

    Add project files.

---
+++ Controllers/TaskListsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GC_TA_TaskList.Models;
+
+namespace GC_TA_TaskList.Controllers
+{
+    public class TaskListsController : Controller
+    {
+        private GrandCircusTaskListEntities db = new GrandCircusTaskListEntities();
+
+        // GET: TaskLists
+            public ViewResult Index(string sortOrder, string searchString)
+            {
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            var tasks = from s in db.TaskLists
+                               select s;
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    tasks = tasks.Where(s => s.TaskName.Contains(searchString)
+                                           || s.TaskDescription.Contains(searchString));
+                }
+                return View(tasks.ToList());
+        }
+
+        // GET: TaskLists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskList taskList = db.TaskLists.Find(id);
+            if (taskList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskList);
+        }
+
+        // GET: TaskLists/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TaskLists/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TaskId,TaskName,TaskDueDate,TaskDescription,IsTaskComplete")] TaskList taskList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaskLists.Add(taskList);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taskList);
+        }
+
+        // GET: TaskLists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskList taskList = db.TaskLists.Find(id);
+            if (taskList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskList);
+        }
+
+        // POST: TaskLists/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TaskId,TaskName,TaskDueDate,TaskDescription,IsTaskComplete")] TaskList taskList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taskList).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taskList);
+        }
+
+        // GET: TaskLists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskList taskList = db.TaskLists.Find(id);
+            if (taskList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskList);
+        }
+
+        // POST: TaskLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TaskList taskList = db.TaskLists.Find(id);
+            db.TaskLists.Remove(taskList);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GC_TA_TaskList.MvcApplication" Language="C#" %>
